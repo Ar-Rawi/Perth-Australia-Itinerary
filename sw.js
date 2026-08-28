35e4feabaa0ead4f09a5028fc12f81f25c8c9cd9
Ar-Rawi <ar-rawi@users.noreply.github.com>
2026-08-29 07:42:54 +0800

    Initial release of reactive Perth Itinerary PWA with table-centric UI, custom themed time pickers, and offline support

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'perth-itinerary-v2';
+const ASSETS = [
+  './',
+  './Perth_Itinerary_V2.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch((err) => console.log('Cache addAll error:', err));
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).catch(() => caches.match('./Perth_Itinerary_V2.html'));
+    })
+  );
+});
